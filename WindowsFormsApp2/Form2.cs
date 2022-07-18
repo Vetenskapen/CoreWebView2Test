fc317d5a8cf14e913d8156af80528f37b699012b
Vetenskapen <76519647+Vetenskapen@users.noreply.github.com>
2022-07-18 14:40:00 +0200

    Update Form2.cs

--- WindowsFormsApp2/Form2.cs
+++ WindowsFormsApp2/Form2.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Core;
 using System;
 using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
@@ -15,7 +16,8 @@ namespace WindowsFormsApp2
             this.Resize += new EventHandler(this.Form_Resize);
             //starting page
             this.webView.Source = new Uri("https://google.se");
-            this.webView.Visible = false;
+            this.webView.Visible = true;
+            UthoppAsync();
         }
 
 
@@ -42,27 +44,29 @@ namespace WindowsFormsApp2
 
         private void btnForward_Click(object sender, EventArgs e)
         {
-            Uthopp();
+            
             if (webView.CanGoForward)
             {
                 webView.GoForward();
             }
         }
 
-        private  void Uthopp()
+        private async Task UthoppAsync()
         {
-          
+            
             string url = "https://www.w3schools.com/action_page.php";
             var postData = "fname=test&lname=test";
             //måste göra om postdata till system io stream för att köra metoden webresource request
             System.IO.MemoryStream postData2 = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(postData));
-
+            
+            //Väntar in navigation
+            await NavigateTo(url, webView);
+            // efter faställt att navigation finns, skickar parametrar
             var request = webView.CoreWebView2.Environment.CreateWebResourceRequest(url,
               "POST", postData2, "Content-Type: application/x-www-form-urlencoded");
             webView.CoreWebView2.NavigateWithWebResourceRequest(request);
-
-            this.webView.Visible = true;
-
         }
+        //Metod för att navigera till url. Async används för att faställa att vi har laddat in allt.
+        public async Task NavigateTo(string url, Microsoft.Web.WebView2.WinForms.WebView2 webb) { await webb.EnsureCoreWebView2Async(); webb.Source = new Uri(url); }
     }
 }
