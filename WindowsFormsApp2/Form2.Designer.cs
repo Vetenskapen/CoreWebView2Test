09ff28bee3c3e4b4e911795b73a380f82e963475
Emil1 <Emil1@RAZER>
2022-07-18 12:53:01 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form2.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnForward = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(1, 37);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(802, 414);
+            this.webView.TabIndex = 0;
+            this.webView.Visible = false;
+            this.webView.ZoomFactor = 1D;
+            this.webView.Click += new System.EventHandler(this.webView_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnBack.Location = new System.Drawing.Point(0, 0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 38);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnForward);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 38);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(97, 0);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(99, 38);
+            this.btnForward.TabIndex = 2;
+            this.btnForward.Text = ">\r\n";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webView);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnForward;
+    }
+}
---
+++ WindowsFormsApp2/Form2.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            //Lyssnar på att göra fönstret större / mindre
+            this.Resize += new EventHandler(this.Form_Resize);
+            //starting page
+            this.webView.Source = new Uri("https://google.se");
+            this.webView.Visible = false;
+        }
+
+
+        //Koden under låter webbläsaren följa storleken på fönstret
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new Size(webView.Location);
+
+        }
+
+        private void webView_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //koden under är för navigation 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            Uthopp();
+            if (webView.CanGoForward)
+            {
+                webView.GoForward();
+            }
+        }
+
+        private  void Uthopp()
+        {
+          
+            string url = "https://www.w3schools.com/action_page.php";
+            var postData = "fname=test&lname=test";
+            //måste göra om postdata till system io stream för att köra metoden webresource request
+            System.IO.MemoryStream postData2 = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(postData));
+
+            var request = webView.CoreWebView2.Environment.CreateWebResourceRequest(url,
+              "POST", postData2, "Content-Type: application/x-www-form-urlencoded");
+            webView.CoreWebView2.NavigateWithWebResourceRequest(request);
+
+            this.webView.Visible = true;
+
+        }
+    }
+}
